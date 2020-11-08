a4e9188669378b526459862504220363fb09a424
Georgi Barnev <gbarnev@gmail.com>
2020-11-09 00:45:23 +0200

    Fix algorithm, refactor

--- SlidingPuzzle/SlidingPuzzle/Program.cs
+++ SlidingPuzzle/SlidingPuzzle/Program.cs
@@ -4,225 +4,117 @@ using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Linq;
 using Priority_Queue;
+using System.Diagnostics;
 
 namespace SlidingPuzzle
 {
     public class Program
     {
-        public enum Direction
-        {
-            Up = 0,
-            Down = 1,
-            Left = 2,
-            Right = 3,
-        }
-
-        public class Table
-        {
-            private int? manhattanDistance;
-            private byte[,] representation;
-            private string cachedStringRepr;
-            private Table cameFrom;
-            private byte zeroRow, zeroCol;
-
-            public Table(byte[,] representation, Table cameFrom)
-            {
-                this.cameFrom = cameFrom;
-                this.representation = representation;
-                (this.zeroRow, this.zeroCol) = this.FindTargetCoordinatesInMattrix(0, representation);
-            }
-
-            public Table Move(Direction direction)
-            {
-                if (direction == Direction.Up && this.zeroRow < this.representation.GetLength(0) - 1)
-                {
-                    byte[,] newRepr = (byte[,])this.representation.Clone();
-                    this.Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow + 1, this.zeroCol]);
-                    return new Table(newRepr, this);
-                }
-
-                if (direction == Direction.Down && this.zeroRow > 0)
-                {
-                    byte[,] newRepr = (byte[,])this.representation.Clone();
-                    this.Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow - 1, this.zeroCol]);
-                    return new Table(newRepr, this);
-                }
-
-                if (direction == Direction.Left && this.zeroCol < this.representation.GetLength(1) - 1)
-                {
-                    byte[,] newRepr = (byte[,])this.representation.Clone();
-                    this.Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow, this.zeroCol + 1]);
-                    return new Table(newRepr, this);
-                }
-
-                if (direction == Direction.Right && this.zeroCol > 0)
-                {
-                    byte[,] newRepr = (byte[,])this.representation.Clone();
-                    this.Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow, this.zeroCol - 1]);
-                    return new Table(newRepr, this);
-                }
-                return null;
-            }
-
-            public Table LastTable => this.cameFrom;
-
-            //public int ManhattanDistance
-            //{
-            //    get
-            //    {
-            //        if (!this.manhattanDistance.HasValue)
-            //        {
-            //            this.manhattanDistance = this.CalcManhattanDistance();
-            //        }
-            //        return this.manhattanDistance.Value;
-            //    }
-            //}
-
-            public override string ToString()
-            {
-                if (string.IsNullOrEmpty(this.cachedStringRepr))
-                {
-                    var stringRepr = new StringBuilder();
-                    for (int i = 0; i < representation.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < representation.GetLength(1); j++)
-                        {
-                            stringRepr.Append($"{representation[i, j]} ");
-                        }
-                        stringRepr.Append('\n');
-                    }
-                    this.cachedStringRepr = stringRepr.ToString();
-                }
-
-                return this.cachedStringRepr;
-            }
-
-            public int CalcManhattanDistance(Table table)
-            {
-                int total = 0;
-                int kx, ky = 0;
-                int expX, expY = 0;
-                var colsInRepr = this.representation.GetLength(1);
-                for (byte k = 1; k < this.representation.Length; k++)
-                {
-
-                    (expX, expY) = FindTargetCoordinatesInMattrix(k, table.representation);
-                    (kx, ky) = FindTargetCoordinatesInMattrix(k, this.representation);
-                    total = total + Math.Abs(kx - expX) + Math.Abs(ky - expY);
-                }
-                return total;
-            }
-
-            private (byte, byte) FindTargetCoordinatesInMattrix(byte target, byte[,] mattrix)
-            {
-                for (byte i = 0; i < mattrix.GetLength(0); i++)
-                {
-                    for (byte j = 0; j < mattrix.GetLength(1); j++)
-                    {
-                        if (mattrix[i, j] == target)
-                        {
-                            return (i, j);
-                        }
-                    }
-                }
-                return (0, 0);
-            }
-
-            private void Swap(ref byte x, ref byte y)
-            {
-                var temp = x;
-                x = y;
-                y = temp;
-            }
-        }
-
-        public class TableEqualityComparer : IEqualityComparer<Table>
-        {
-            public bool Equals([AllowNull] Table x, [AllowNull] Table y)
-            {
-                return x.ToString() == y.ToString();
-            }
-
-            public int GetHashCode([DisallowNull] Table table)
-            {
-                return table.ToString().GetHashCode();
-            }
-        }
-
         static void Main(string[] args)
         {
-            byte[,] testTable =
+            var (startArr, targetArr) = ParseInitialAndTargetTables();
+            var startState = new Table(startArr, null, null, 0);
+            var targetTable = new Table(targetArr, null, null, 0);
+            var stack = new Stack<Table>();
+            Console.WriteLine("Begin processing...");
+            var sw = new Stopwatch();
+            sw.Start();
+            var curBound = startState.CalcManhattanDistance(targetTable);
+            while (!IdsSearch(startState, targetTable, curBound, out var nextBound, stack))
             {
-                {15, 14, 1, 6},
-                {9, 11, 4, 12},
-                {0, 10, 7, 3},
-                {13, 8, 5, 2},
-            };
+                curBound = nextBound;
+            }
+            sw.Stop();
+            Console.WriteLine($"Finished for {sw.ElapsedMilliseconds}ms");
+        }
 
-            var targetTable = new Table(new byte[,]{
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 0},
-            }, null);
+        public static bool IdsSearch(Table initialState, Table targetState, int bound, out int nextBound, Stack<Table> stack)
+        {
+            //Console.WriteLine($"Trying with bound: {bound}");
+            nextBound = int.MaxValue;
+            stack.Push(initialState);
 
-
-
-            var startState = new Table(testTable, null);
-            Console.WriteLine("initial manhattan distance: " + startState.CalcManhattanDistance(targetTable));
-            var itemEqualityComparer = new TableEqualityComparer();
-
-            var priorityQueue = new SimplePriorityQueue<Table, int>(itemEqualityComparer);
-            var visitedStates = new List<string>();
-
-
-            for (int threshold = 100; threshold < 10000000; threshold *= 2)
+            while (stack.Count > 0)
             {
-                Console.WriteLine($"Trying with threshold: {threshold}");
-                var pathLength = 0;
-                priorityQueue.Enqueue(startState, startState.CalcManhattanDistance(targetTable));
-                visitedStates.Add(startState.ToString());
-                while (priorityQueue.Count > 0 && pathLength <= threshold)
+                var cur = stack.Pop();
+                var curManhattan = cur.CalcManhattanDistance(targetState);
+                if (curManhattan == 0)
                 {
-                    var cur = priorityQueue.Dequeue();
-                    //Console.WriteLine(cur);
+                    PrintSolution(cur);
+                    return true;
+                }
 
-                    if (cur.ToString() == targetTable.ToString())
+                var fn = curManhattan + cur.PathLength;
+                if (bound < fn)
+                {
+                    if (nextBound > fn)
                     {
-                        Console.WriteLine("WIN!!");
-
-                        Console.WriteLine("========= AND NOW ============");
-                        Console.WriteLine("========= THE SOLUTION ============");
-                        Console.WriteLine(cur);
-                        var last = cur.LastTable;
-                        while (last != null)
-                        {
-                            Console.WriteLine(last);
-                            last = last.LastTable;
-                        }
-                        break;
+                        nextBound = fn;
                     }
+                    continue;
+                }
 
-                    pathLength++;
-                    for (int i = 0; i < 4; i++)
+                for (int i = 0; i < 4; i++)
+                {
+                    var successor = cur.Move((Direction)i);
+                    if (successor != null)
                     {
-                        var neighbour = cur.Move((Direction)i);
-                        if (neighbour != null && !visitedStates.Contains(neighbour.ToString()))
-                        {
-                            //Console.WriteLine(
-                            //    $"neighbour in direction {(Direction)i} has value {neighbour.CalcManhattanDistance(targetTable)} + {pathLength}");
-                            priorityQueue.Enqueue(neighbour, neighbour.CalcManhattanDistance(targetTable) + pathLength);
-                            visitedStates.Add(neighbour.ToString());
-                        }
+                        stack.Push(successor);
                     }
                 }
-                pathLength = 0;
-                priorityQueue.Clear();
-                visitedStates.Clear();
             }
-            Console.ReadKey();
+            return false;
+        }
+
+        private static (byte[,], byte[,]) ParseInitialAndTargetTables()
+        {
+            var tableLength = int.Parse(Console.ReadLine());
+            var tableDim = (int)Math.Sqrt(tableLength + 1);
+            byte[,] startArr = new byte[tableDim, tableDim];
+            byte[,] targetArr = new byte[tableDim, tableDim];
+            var pos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tableDim; i++)
+            {
+                var curLine = Console.ReadLine();
+                byte[] integers = curLine.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => byte.Parse(s)).ToArray();
+                for (int j = 0; j < tableDim; j++)
+                {
+                    startArr[i, j] = integers[j];
+                }
+            }
+
+            var posZero = (tableLength + 1 + pos) % (tableLength + 1);
+            byte counter = 1;
+            for (int i = 0; i < tableDim; i++)
+            {
+                for (int j = 0; j < tableDim; j++)
+                {
+                    if (i == posZero / tableDim && j == posZero % tableDim)
+                    {
+                        continue;
+                    }
+                    targetArr[i, j] = counter;
+                    counter++;
+                }
+            }
+
+            return (startArr, targetArr);
+        }
+
+        private static void PrintSolution(Table current)
+        {
+            var results = new List<string>();
+            results.Add(current.CameFrom.ToString());
+            var last = current.LastTable;
+            while (last != null && last.CameFrom != null)
+            {
+                results.Add(last.CameFrom.ToString());
+                last = last.LastTable;
+            }
+            results.Reverse();
+            Console.WriteLine(results.Count);
+            Console.WriteLine(string.Join('\n', results));
         }
     }
-
-
 }
---
+++ SlidingPuzzle/SlidingPuzzle/Table.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlidingPuzzle
+{
+    public enum Direction
+    {
+        Up = 2,
+        Down = 3,
+        Left = 0,
+        Right = 1
+    }
+    public class Table
+    {
+        private byte[,] representation;
+        private byte zeroRow, zeroCol;
+
+        public Table(byte[,] representation, Table lastTable, Direction? lastDirection, int pathLength)
+        {
+            this.LastTable = lastTable;
+            this.CameFrom = lastDirection;
+            this.PathLength = pathLength;
+            this.representation = representation;
+            (this.zeroRow, this.zeroCol) = this.FindTargetCoordinatesInMattrix(0, representation);
+        }
+
+        public Table Move(Direction direction)
+        {
+            if (direction == Direction.Up &&
+                (!this.CameFrom.HasValue || this.CameFrom.Value != Direction.Down) &&
+                this.zeroRow < this.representation.GetLength(0) - 1)
+            {
+                byte[,] newRepr = (byte[,])this.representation.Clone();
+                Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow + 1, this.zeroCol]);
+                return new Table(newRepr, this, direction, this.PathLength + 1);
+            }
+
+            if (direction == Direction.Down &&
+                (!this.CameFrom.HasValue || this.CameFrom.Value != Direction.Up) &&
+                this.zeroRow > 0)
+            {
+                byte[,] newRepr = (byte[,])this.representation.Clone();
+                Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow - 1, this.zeroCol]);
+                return new Table(newRepr, this, direction, this.PathLength + 1);
+            }
+
+            if (direction == Direction.Left &&
+                (!this.CameFrom.HasValue || this.CameFrom.Value != Direction.Right) &&
+                this.zeroCol < this.representation.GetLength(1) - 1)
+            {
+                byte[,] newRepr = (byte[,])this.representation.Clone();
+                Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow, this.zeroCol + 1]);
+                return new Table(newRepr, this, direction, this.PathLength + 1);
+            }
+
+            if (direction == Direction.Right &&
+                (!this.CameFrom.HasValue || this.CameFrom.Value != Direction.Left) &&
+                this.zeroCol > 0)
+            {
+                byte[,] newRepr = (byte[,])this.representation.Clone();
+                Swap(ref newRepr[this.zeroRow, this.zeroCol], ref newRepr[this.zeroRow, this.zeroCol - 1]);
+                return new Table(newRepr, this, direction, this.PathLength + 1);
+            }
+            return null;
+        }
+
+        public int PathLength { get; }
+
+        public Table LastTable { get; }
+
+        public Direction? CameFrom { get; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < representation.GetLength(0); i++)
+            {
+                for (int j = 0; j < representation.GetLength(1); j++)
+                {
+                    sb.Append($"{representation[i, j]} ");
+                }
+                sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+
+        public int CalcManhattanDistance(Table target)
+        {
+            int total = 0;
+            int kx, ky = 0;
+            int expX, expY = 0;
+            var colsInRepr = this.representation.GetLength(1);
+            for (byte k = 1; k < this.representation.Length; k++)
+            {
+                (expX, expY) = FindTargetCoordinatesInMattrix(k, target.representation);
+                (kx, ky) = FindTargetCoordinatesInMattrix(k, this.representation);
+                total = total + Math.Abs(kx - expX) + Math.Abs(ky - expY);
+            }
+            return total;
+        }
+
+        private (byte, byte) FindTargetCoordinatesInMattrix(byte target, byte[,] mattrix)
+        {
+            for (byte i = 0; i < mattrix.GetLength(0); i++)
+            {
+                for (byte j = 0; j < mattrix.GetLength(1); j++)
+                {
+                    if (mattrix[i, j] == target)
+                    {
+                        return (i, j);
+                    }
+                }
+            }
+            return (0, 0);
+        }
+
+        private static void Swap(ref byte x, ref byte y)
+        {
+            var temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
